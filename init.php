ee41eb1d0cced7984795f9025e4aa4a2bedbcd8f
Miłosz <milosz+github@sieradzki.it>
2014-07-13 15:54:17 +0200

    Fix #1: Facebook changed URL for images

--- init.php
+++ init.php
@@ -24,15 +24,46 @@ class Af_FacebookFeedImages extends Plugin {
 	function hook_article_filter($article) {
 		$owner_uid = $article["owner_uid"];
 
-		if (strpos($article["link"], "://www.facebook.com/") !== FALSE) {
-			if (strpos($article["plugin_data"], "facebookfeedimages,$owner_uid:") === FALSE) {
-				if (strpos($article["content"], '_s.jpg"') !== FALSE) {
-					$article["content"] = str_replace('_s.jpg"', '_n.jpg"', $article["content"]);
-					$article["plugin_data"] = "facebookfeedimages,$owner_uid:" . $article["plugin_data"];
-				}
-			} else if (isset($article["stored"]["content"])) {
+		if (strpos($article["link"], "://www.facebook.com/") === FALSE) {
+			return $article;
+		}
+
+		if (strpos($article["plugin_data"], "facebookfeedimages,$owner_uid:") !== FALSE) {
+			if (isset($article["stored"]["content"])) {
 				$article["content"] = $article["stored"]["content"];
 			}
+
+			return $article;
+		}
+
+
+		$doc = new DOMDocument();
+		@$doc->loadHTML('<?xml encoding="UTF-8"?>' . $article["content"]);
+		if (!$doc) {
+			return $article;
+		}
+
+		$found_images = FALSE;
+		$xpath = new DOMXPath($doc);
+		$images = $xpath->query('//img');
+		if (empty($images)) {
+			return $article;
+		}
+
+		foreach ($images as $image) {
+			$image_src = $image->getAttribute("src");
+			if ((strpos($image_src, ".fbcdn.net/") !== FALSE || strpos($image_src, ".akamaihd.net/") !== FALSE)
+				&& strpos($image_src, "/s130x130/") !== FALSE) {
+
+				$image_src = str_replace('/s130x130/', '/', $image_src);
+				$image->setAttribute("src", $image_src);
+				$found_images = TRUE;
+			}
+		}
+
+		if ($found_images) {
+			$article["content"] = $doc->saveHTML();
+			$article["plugin_data"] = "facebookfeedimages,$owner_uid:" . $article["plugin_data"];
 		}
 
 		return $article;
